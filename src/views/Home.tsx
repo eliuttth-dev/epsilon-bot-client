859e6b3d5bc37b9bafd6183960f25027b76b3f71
eliuttth-dev <eliuttth@gmail.com>
2025-07-19 22:16:13 +0200

    border deleted

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,7 @@ export const HomeView = () => {
   return (
     <>
       <main className="w-full h-full bg-ocean-700 text-white flex items-center justify-center p-4">
-        <section className="w-full p-4 flex items-center justify-center border">
+        <section className="w-full p-4 flex items-center justify-center ">
           <div className=" flex flex-col items-center justify-center gap-[20px]">
             <h1 className="font-bold text-4xl text-center">
               What do you need?
